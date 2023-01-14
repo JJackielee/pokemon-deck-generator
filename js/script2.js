ae2fe8ae048242d4ca99745014ad5d81bc75c257
Herman <evanaherman@gmail.com>
2023-01-14 09:48:08 -0800

    updates

--- js/script2.js
+++ js/script2.js
@@ -1,7 +1,11 @@
 var btn = document.querySelector("#pokemon-button")
 btn.addEventListener("click", function(){
+    var deckType = document.querySelector('input[name="deckType"]:checked').value;
     var deckName = document.querySelector("#deck-name").value;
     localStorage.setItem("Deck Name", deckName)
     console.log(localStorage)
-    
+    console.log(deckType)
+
+
 })
+
